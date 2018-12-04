e3bd4bc2d26a408bfb8ebc48e2122ed80884ad4c
juref <jure.filipic@gmail.com>
2018-12-04 09:14:14 +0100

    typo :)

--- casino.py
+++ casino.py
@@ -6,14 +6,14 @@ class textColor:
     BOLD = '\033[01m'
     GREEN = '\033[32m'
     ORANGE = '\033[33m'
-    BLACK = '\033[90m'
+    GREY = '\033[90m'
 
 from decimal import Decimal
 import random
 from os import system
 
 skrita_stevilka = round(random.uniform(1, 30))
-print textColor.BLACK + "\nZa lažje ugibanje, " + str(skrita_stevilka).rstrip('0').rstrip('.') + " je skrita številka." + textColor.RESET  if '.' in str(skrita_stevilka) else str(skrita_stevilka) + " je skrita številka." + textColor.RESET
+print textColor.GREY + "\nZa lažje ugibanje, " + str(skrita_stevilka).rstrip('0').rstrip('.') + " je skrita številka." + textColor.RESET  if '.' in str(skrita_stevilka) else str(skrita_stevilka) + " je skrita številka." + textColor.RESET
 
 while True:
     odgovor = int(raw_input("\nUgani skrito številko med 1 in 30: "))
@@ -24,7 +24,7 @@ while True:
         break
     else:
         # system('clear') # dela samo v terminalu za mac in linux!!!
-        print textColor.ORANGE + "\nŽal " + str(odgovor) + " ni pravilen odgovor. Prosim poiskusite ponovno!" + textColor.RESET
+        print textColor.ORANGE + "\nŽal " + str(odgovor) + " ni pravilen odgovor. Prosim poskusite ponovno!" + textColor.RESET
 
 
 str(skrita_stevilka).rstrip('0').rstrip('.') if '.' in str(skrita_stevilka) else str(skrita_stevilka)
