5b702df0a70b8e13c73729840e671f87c87f35cc
juref <jure.filipic@gmail.com>
2018-12-06 16:53:43 +0100

    Nekaj popravkov

--- casino_v2.0.py
+++ casino_v2.0.py
@@ -10,7 +10,7 @@ class textColor:
 
 
 import random
-from os import system
+import os
 
 skrita_stevilka = round(random.uniform(1, 30))
 print textColor.GREY + "\nZa lažje ugibanje, " + str(skrita_stevilka).rstrip('0').rstrip('.') + " je skrita številka." + textColor.RESET  if '.' in str(skrita_stevilka) else str(skrita_stevilka) + " je skrita številka." + textColor.RESET
@@ -20,16 +20,17 @@ poskus = 0
 while poskus < 3:
     se_poskus = 3 - poskus
     odgovor = str(raw_input("\nUgani skrito številko med 1 in 30 (poskusiš lahko še " + str(se_poskus) + "x): "))
-    if int(odgovor) == skrita_stevilka:
-        system('clear')  # dela samo v terminalu za mac in linux!!!
-        print textColor.BOLD + textColor. GREEN + "\nČestitamo, " + str(skrita_stevilka).rstrip('0').rstrip('.') + " je pravilni odgovor!" + textColor.RESET if '.' in str(skrita_stevilka) else str(skrita_stevilka) + "je pravilni odgovor!" + textColor.RESET
-        break
-    elif odgovor.isalpha() or len(odgovor) == 0 or " " in odgovor or "+" in odgovor or "-" in odgovor or "*" in odgovor or "/" in odgovor:
+    if odgovor.isalpha() or len(odgovor) == 0 or " " in odgovor or "+" in odgovor or "-" in odgovor or "*" in odgovor or "/" in odgovor:
         print "\033[91mTo ni število\033[0m"
+    elif int(odgovor) == skrita_stevilka:
+        os.system('clear')  # dela samo v terminalu za mac in linux!!!
+        print textColor.BOLD + textColor. GREEN + "\nČestitamo, " + str(skrita_stevilka).rstrip('0').rstrip('.') + " je pravilni odgovor!" + textColor.RESET if '.' in str(skrita_stevilka) else str(skrita_stevilka) + "je pravilni odgovor!" + textColor.RESET
+        os.system('afplay sound.mp3')
+        break
     elif poskus == 2:
         print textColor.ORANGE + "\nŽal " + str(odgovor) + " ni pravilen odgovor. Hvala za igro!" + textColor.RESET
         poskus += 1
     else:
-        system('clear') # dela samo v terminalu za mac in linux!!!
+        os.system('clear') # dela samo v terminalu za mac in linux!!!
         print textColor.ORANGE + "\nŽal " + str(odgovor) + " ni pravilen odgovor. Prosim poskusite ponovno!" + textColor.RESET
         poskus += 1
